e373f31810d7c6de6d6f8de548be09fe21aec5c7
Eugene Solodovnykov <eugene.solodovnykov@gmail.com>
2013-05-13 19:42:48 +0200

    Rewritten using Security.framework to decode mobileprovision file
    By fact, mobileprovision profile is PKCS#7 signed plist. So it's better to use proper tool to work with it.
    Security.framework usage helps to decode signed plist (i.e. to clean it up). That approach looks much better then NSScanner usage.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-IMPORTANT
+How to easily build it
 =====================
 
-That version is quite old and is outdated.
-Please use [that gist](https://gist.github.com/sharpland/5134242) instead. Believe me, it's much better, it also allows you to use key-path to retrieve profile properties.
+Just run command below in your terminal:
 
-You might use the command below to build the app. It would download the source, compile it and put the resulting binary to /usr/local/bin/mobileprovision-read
+    curl https://raw.github.com/sharpland/mobileprovisionParser/master/main.m | gcc -framework Foundation -framework Security -o /usr/local/bin/mobileprovision-read -x objective-c -
 
-    curl https://raw.github.com/gist/5134242 | gcc -framework Foundation -framework Security -o /usr/local/bin/mobileprovision-read -x objective-c -
+This command would download the source, compile it and put resulting binary to /usr/local/bin/mobileprovision-read
+
+Run `mobileprovision-read` without any parameter to get short help.
 
---
+++ main.m
@@ -0,0 +1,100 @@
+#import <Foundation/Foundation.h>
+#import <Security/Security.h>
+ 
+int main(int argc, const char *argv[]) {
+    NSUserDefaults *arguments = [NSUserDefaults standardUserDefaults];
+    NSString *file = [arguments stringForKey:@"f"];
+    NSString *option = [arguments stringForKey:@"o"];
+ 
+    if (!file) {
+        printf("\
+\033[1m%1$s\033[0m -- mobileprovision files querying tool.\n\
+\n\
+\033[1mUSAGE\033[0m\n\
+\033[1m%1$s\033[0m \033[1m-f\033[0m \033[4mfileName\033[0m [\033[1m-o\033[0m \033[4moption\033[0m]\n\n\
+\033[1mOPTIONS\033[0m\n\
+    \033[1mtype\033[0m – prints mobileprovision profile type (debug, ad-hoc, enterprise, appstore)\n\
+    \033[1mappid\033[0m – prints application identifier\n\
+Will print raw provision's plist if option is not specified.\n\
+You can also use \033[1mkey path\033[0m as an option.\n\
+\n\
+\033[1mEXAMPLES\033[0m\n\
+%1$s -f test.mobileprovision -o type\n\
+    Prints profile type\n\
+\n\
+%1$s -f test.mobileprovision -o UUID\n\
+    Prints profile UUID\n\
+\n\
+%1$s -f test.mobileprovision -o ProvisionedDevices\n\
+    Prints provisioned devices UDIDs\n\
+\n\
+%1$s -f test.mobileprovision -o Entitlements.get-task-allow\n\
+    Prints 0 if profile doesn't allow debugging 1 otherwise\n\
+", argv[0]);
+        return 1001;
+    }
+ 
+    CMSDecoderRef decoder = NULL;
+    CFDataRef dataRef = NULL;
+    NSString *plistString = nil;
+ 
+    @try {
+        CMSDecoderCreate(&decoder);
+        NSData *fileData = [NSData dataWithContentsOfFile:file];
+        CMSDecoderUpdateMessage(decoder, fileData.bytes, fileData.length);
+        CMSDecoderFinalizeMessage(decoder);
+        CMSDecoderCopyContent(decoder, &dataRef);
+        plistString = [[NSString alloc] initWithData:(NSData *)dataRef encoding:NSUTF8StringEncoding];
+    }
+    @catch (NSException *exception) {
+        @throw exception;
+    }
+    @finally {
+        if (decoder) CFRelease(decoder);
+        if (dataRef) CFRelease(dataRef);
+    }
+ 
+    if (plistString) {
+        NSDictionary *plist = [plistString propertyList];
+ 
+        if (!option) {
+            printf("%s", [plistString UTF8String]);
+        }
+        if ([option isEqualToString:@"type"]) {
+            if ([plist valueForKeyPath:@"ProvisionedDevices"]){
+                if ([[plist valueForKeyPath:@"Entitlements.get-task-allow"] boolValue]) {
+                    printf("debug\n");
+                }
+                else {
+                    printf("ad-hoc\n");
+                }
+            }
+            else if ([[plist valueForKeyPath:@"ProvisionsAllDevices"] boolValue]) {
+                    printf("enterprise\n");
+            }
+            else {
+                    printf("appstore\n");
+            }
+        }
+        else if ([option isEqualToString:@"appid"]) {
+            NSString *applicationIdentifier = [plist valueForKeyPath:@"Entitlements.application-identifier"];
+            NSString *prefix = [[[plist valueForKeyPath:@"ApplicationIdentifierPrefix"] objectAtIndex:0] stringByAppendingString:@"."];
+            printf("%s\n", [[applicationIdentifier stringByReplacingOccurrencesOfString:prefix withString:@""] UTF8String]);
+        }
+        else {
+            id result = [plist valueForKeyPath:option];
+            if (result) {
+                if ([result isKindOfClass:[NSArray class]] && [result count]) {
+                    printf("%s\n", [[result componentsJoinedByString:@"\n"] UTF8String]);
+                }
+                else {
+                    printf("%s\n", [[result description] UTF8String]);
+                }
+            }
+        }
+    }
+ 
+    [plistString release];
+ 
+    return 0;
+}
